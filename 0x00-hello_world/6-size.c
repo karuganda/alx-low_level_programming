1d5b340a06da56984bd329948bf9fbf51d62e901
karuganda <karugandafrank234@gmail.com>
2022-06-02 15:11:07 -0700

    added the first files

---
+++ 0x00-hello_world/6-size.c
@@ -0,0 +1,14 @@
+#include <stdio.h>
+/**
+ * main - main block
+ * Return: 0
+ */
+int main(void)
+{
+	printf("Sinze of a char: %i byte(s)\n", sizeof(char));
+	printf("Sinze of an int: %i byte(s)\n", sizeof(int));
+	printf("Size of a long int: %i byte(s)\n", sizeof(long int));
+	printf("Size of a long long int: %i byte(s)\n", sizeof(long long int));
+	print("Size of a dloat: %i byte(s)\n", sizeof (float));
+	return(0);
+}
