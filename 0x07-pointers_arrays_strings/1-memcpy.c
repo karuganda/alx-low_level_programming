81223090c77524da54ef5765dfa1ac6f8a565528
karuganda <karugandafrank234@gmail.com>
2022-06-20 07:11:41 -0700

    cor 1-memcpy

--- 0x07-pointers_arrays_strings/1-memcpy.c
+++ 0x07-pointers_arrays_strings/1-memcpy.c
@@ -1,9 +1,11 @@
 #include "main.h"
-
 /**
- * _memcpy - 
- */
-
+* _memcpy - description 
+* @dest: description
+* @src: description
+* @n: description
+* Return: return value
+*/
 char *_memcpy(char *dest, char *src, unsigned int n)
 {
 	unsigned int i = 0;
